355237054413b93367ec6b22aaf746c4d069d37e
mbatuhanozdemir <mbatuhanozdemir@hotmail.com>
2023-01-02 22:57:42 +0300

    Yüklendi

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int time = DateTime.Now.Hour;
+
+if (time >=6 && time<12)
+    Console.WriteLine("Günaydın");
+else if (time <= 18 )
+    Console.WriteLine("İyi Günler");
+else
+    Console.WriteLine("İyi Geceler");
+
+string sonuc = time <= 18 ? "İyi günler" : "İyi Geceler ";
+
+ sonuc = time >=6 && time <12 ? "Günaydın ! " :  time<=18 ? "İyi Günler! " : "İyi geceler! ";
+
+    Console.WriteLine(sonuc);
